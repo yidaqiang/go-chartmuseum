f834902c84452dd9467699ef3fa5aabea40a91eb
Vista Yih <daqiang.yi@hand-china.com>
2020-12-24 15:42:24 +0800

    add test case use basic auth

--- chartmuseum/chart_test.go
+++ chartmuseum/chart_test.go
@@ -3,16 +3,18 @@ package chartmuseum
 import (
 	"context"
 	helm_repo "helm.sh/helm/v3/pkg/repo"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"testing"
 )
 
 func TestChartService_ListCharts(t *testing.T) {
-	testCase := []struct{
-		Path string
+	testCase := []struct {
+		Path   string
 		Result map[string]helm_repo.ChartVersions
-	} {
+	}{
 		{
 			"c7n/test",
 			map[string]helm_repo.ChartVersions{},
@@ -43,7 +45,6 @@ func TestChartService_DeleteChart(t *testing.T) {
 func TestChartService_UploadChart(t *testing.T) {
 	client, _ := NewClient("http://chart.example.com", nil)
 
-
 	filepath.Walk("/tmp/chart", func(path string, info os.FileInfo, err error) error {
 		if info == nil {
 			return err
@@ -59,4 +60,23 @@ func TestChartService_UploadChart(t *testing.T) {
 		t.Log(resp.StatusCode)
 		return nil
 	})
+}
+
+func TestChartService_ListCharts2(t *testing.T) {
+	repo := "org-review/proj-review"
+	username := "root"
+	password := "handhand"
+
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			Proxy: func(req *http.Request) (*url.URL, error) {
+				req.SetBasicAuth(username, password)
+				return nil, nil
+			},
+		},
+	}
+	client, _ := NewClient("http://chartmuseum.c7n.devops.hand-china.com", httpClient)
+	result := map[string]helm_repo.ChartVersions{}
+	resp, _ := client.ChartService.ListCharts(context.Background(), repo, &result)
+	t.Log(resp.Message)
 }
